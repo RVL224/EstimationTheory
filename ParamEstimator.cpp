dc3319c047cb9e9166f372aaa398a97509d2aac8
ycxxn <kevin8401128@gmail.com>
2020-04-27 15:44:50 +0800

    param upgrade

--- ParamEstimator.cpp
+++ ParamEstimator.cpp
@@ -6,22 +6,28 @@ using namespace std;
 
 CParamEstimator::CParamEstimator(void)
 {
-    Float C[9] = {0.9649, 0.9572, 0.1419, 
-				  0.1576, 0.4854, 0.4218, 
-				  0.9706, 0.8003, 0.9157};
-    fMatrix a(C,3,3);
-    cout << "Construct OK" << endl;
+    // cout << "Construct OK" << endl;
 }
 
 CParamEstimator::~CParamEstimator()
 {
 }
 
-// void CParamEstimator::SetParamEstiMethod(ParamEstiMethod Method)
-// {
-//     if (Method == 1)
-//     {
-        
-//     }
-// }
+void CParamEstimator::SetParamEstiMethod(ParamEstiMethod Method)
+{
+    EstiMethod = Method;
+}
+
+void CParamEstimator::SetMethodParameters(ParamEstiMethod Method, void*	pParam)
+{
+    if(Method == LS)
+    {
+        cout << "LS" << endl;
+    }
+}
+
+ParamEstiMethod	CParamEstimator::GetParamEstiMethod(void) const
+{
+    cout << EstiMethod << endl;
+}
 
